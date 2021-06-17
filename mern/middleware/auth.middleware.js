feaa6bf9b293872aa43b3fa718f267fcae598981
Artem Zinovev <artemzinovev688@gmail.com>
2021-06-17 13:30:21 +0300

    merg conf

--- mern/middleware/auth.middleware.js
+++ mern/middleware/auth.middleware.js
@@ -1,43 +1,24 @@
 const jwt = require('jsonwebtoken')
-<<<<<<< HEAD
 const config = require('config')
-=======
-<<<<<<< HEAD
-const config = require('config')
-=======
->>>>>>> 80168492fb305515cc60284b8f378c2c2fecccd7
->>>>>>> 9ef91c50d3980fa45e9c320d0beec77d979362ce
 
 module.exports = (req, res, next) => {
-    if (req.method === 'OPTIONS') {
-        return next()
+  if (req.method === 'OPTIONS') {
+    return next()
+  }
+
+  try {
+
+    const token = req.headers.authorization.split(' ')[1] // "Bearer TOKEN"
+
+    if (!token) {
+      return res.status(401).json({ message: 'Нет авторизации' })
     }
 
-    try {
-        const token = req.headers.authorization.split(' ')[1]
-<<<<<<< HEAD
+    const decoded = jwt.verify(token, config.get('jwtSecret'))
+    req.user = decoded
+    next()
 
-        if (!token) {
-=======
-<<<<<<< HEAD
-
-        if (!token) {
-=======
-        console.log(token)
-        console.log(req.headers)
-
-        if (!token) {
-            console.log('No token')
->>>>>>> 80168492fb305515cc60284b8f378c2c2fecccd7
->>>>>>> 9ef91c50d3980fa45e9c320d0beec77d979362ce
-            return res.status(401).json({ message: 'Нет авторизации' });
-            
-        }
-
-        const decoded = jwt.verify(token, config.get('jwtSecret'))
-        req.user = decoded
-        next()
-    } catch (e) {
-        res.status(401).json({ message: 'Нет авторизации' });
-    }
+  } catch (e) {
+    res.status(401).json({ message: 'Нет авторизации' })
+  }
 }
