e0039d33a876755298716bdb1994fa1895601cac
Amazingwhite <artemzinovev688@gmail.com>
2021-07-15 17:32:40 +0300

    tree на флексах

--- treev2/src/App.js
+++ treev2/src/App.js
@@ -1,56 +1,149 @@
-import './App.css';
+import UserProfile from './components/UserProfile';
+import './styles/App.css';
+import data from './data/data';
+import { useState } from 'react';
+import cn from 'classnames'
 
 function App() {
+
+  let [isActive, setIsActive] = useState(null)
+
+  let setActiveUser = (user) => {
+    setIsActive(user)
+  }
+
   return (
     <div className='tree'>
       <div className="leftSide">
         <div className='oldFather'>
-          <div className='person babushka babushkaFather' style={{marginLeft: '30px'}}></div>
-          <div className='person dedushka dedushkaFather'></div>
+          <UserProfile
+            name={data[4].name}
+            illnesses={data[4].illnesses}
+            id={4}
+            isActive={isActive?.classList.contains('babushkaFather') && isActive}
+            setActiveUser={setActiveUser}
+          />
+          <UserProfile
+            name={data[5].name}
+            illnesses={data[5].illnesses}
+            id={5}
+            isActive={isActive?.classList.contains('dedushkaFather') && isActive}
+            setActiveUser={setActiveUser}
+          />
         </div>
         <div className='connectors'>
           <div className='topConnectors'>
-            <div className='connector topLeft topLeftFather'></div>
-            <div className='connector topRight topRightFather'></div>
+            <div className={cn('connector topLeft topLeftFather', {'activeConnector': data[4].illnesses.length>0})}></div>
+            <div className={cn('connector topRight topRightFather', {'activeConnector': data[5].illnesses.length>0})}></div>
           </div>
           <div className='verticalConnectors'>
-            <div className='connector verticalLeft verticalConnector verticalLeftFather'></div>
-            <div className='connector verticalRight verticalConnector verticalRightFather'></div>
+            <div className={cn('connector verticalLeft verticalConnector verticalLeftFather', {'activeConnector': data[4].illnesses.length>0})}></div>
+            <div className={cn('connector verticalRight verticalConnector verticalRightFather', {'activeConnector': data[5].illnesses.length>0})}></div>
           </div>
           <div className='bottomConnectors'>
-            <div className='connector bottomLeft bottomLeftFather'></div>
-            <div className='connector bottomRight bottomRightFather'></div>
+            <div className={cn('connector bottomLeft bottomLeftFather', {'activeConnector': data[8].illnesses.length>0})}></div>
+            <div className={cn('connector bottomRight bottomRightFather', {'activeConnector': data[4].illnesses.length>0 || data[5].illnesses.length>0 || data[8].illnesses.length>0})}></div>
           </div>
         </div>
         <div className='fatherBlock'>
-          <div className='person siblings' style={{marginLeft: '30px'}}></div>
-          <div className='person father'></div>
+          <UserProfile
+            name={data[8].name}
+            illnesses={data[8].illnesses}
+            id={8}
+            isActive={isActive?.classList.contains('fatherSiblings') && isActive}
+            setActiveUser={setActiveUser}
+          />
+          <UserProfile
+            name={data[2].name}
+            illnesses={data[2].illnesses}
+            id={2}
+            isActive={isActive?.classList.contains('father') && isActive}
+            setActiveUser={setActiveUser}
+          />
         </div>
       </div>
 
       <div className="rightSide">
         <div className='oldMother'>
-          <div className='person babushka babushkaMother'></div>
-          <div className='person dedushka dedushkaMother' style={{marginRight: '30px'}}></div>
+        <UserProfile
+            name={data[6].name}
+            illnesses={data[6].illnesses}
+            id={6}
+            isActive={isActive?.classList.contains('babushkaMother') && isActive}
+            setActiveUser={setActiveUser}
+          />
+          <UserProfile
+            name={data[7].name}
+            illnesses={data[7].illnesses}
+            id={7}
+            isActive={isActive?.classList.contains('dedushkaMother') && isActive}
+            setActiveUser={setActiveUser}
+          />
         </div>
         <div className='connectors'>
           <div className='topConnectors'>
-            <div className='connector topLeft topLeftMother'></div>
-            <div className='connector topRight topRightMother'></div>
+            <div className={cn('connector topLeft topLeftMother', {'activeConnector': data[6].illnesses.length>0})}></div>
+            <div className={cn('connector topRight topRightMother', {'activeConnector': data[7].illnesses.length>0})}></div>
           </div>
           <div className='verticalConnectors'>
-            <div className='connector verticalLeft verticalConnector verticalLeftMother'></div>
-            <div className='connector verticalRight verticalConnector verticalRightMother'></div>
+            <div className={cn('connector verticalLeft verticalConnector verticalLeftMother', {'activeConnector': data[6].illnesses.length>0})}></div>
+            <div className={cn('connector verticalRight verticalConnector verticalRightMother', {'activeConnector': data[7].illnesses.length>0})}></div>
           </div>
           <div className='bottomConnectors'>
-            <div className='connector bottomLeft bottomLeftMother'></div>
-            <div className='connector bottomRight bottomRightMother'></div>
+            <div className={cn('connector bottomLeft bottomLeftMother', {'activeConnector': data[6].illnesses.length>0 || data[7].illnesses.length>0 || data[9].illnesses.length>0})}></div>
+            <div className={cn('connector bottomRight bottomRightMother', {'activeConnector': data[9].illnesses.length>0})}></div>
           </div>
         </div>
         <div className='motherBlock'>
-          <div className='person mother'></div>
-          <div className='person siblings' style={{marginRight: '30px'}}></div>
+          <UserProfile
+            name={data[3].name}
+            illnesses={data[3].illnesses}
+            id={3}
+            isActive={isActive?.classList.contains('mother') && isActive}
+            setActiveUser={setActiveUser}
+          />
+          <UserProfile
+            name={data[9].name}
+            illnesses={data[9].illnesses}
+            id={9}
+            isActive={isActive?.classList.contains('motherSiblings') && isActive}
+            setActiveUser={setActiveUser}
+          />
         </div>
+      </div>
+
+      <div className='bottom'>
+        <div className='selfConnectors'>
+
+          <div className='selfTopConnectors'>
+            <div className='selfLeftConnector activeConnector'></div>
+            <div className='selfRightConnector activeConnector'></div>
+          </div>
+          <div className='selfVerticalConnectors'>
+            <div className='selfVerticalLeft verticalLeft activeConnector'></div>
+            <div className='selfVerticalRight verticalRight activeConnector'></div>
+          </div>
+          
+        </div>
+        
+        <div className='selfBlock'>
+          <UserProfile
+            name={data[1].name}
+            illnesses={data[1].illnesses}
+            id={1}
+            isActive={isActive?.classList.contains('me') && isActive}
+            setActiveUser={setActiveUser}
+          />
+          <div className={cn('horizontalConnector', {'activeConnector': data[10].illnesses.length>0})}></div>
+          <UserProfile
+            name={data[10].name}
+            illnesses={data[10].illnesses}
+            id={10}
+            isActive={isActive?.classList.contains('selfSiblings') && isActive}
+            setActiveUser={setActiveUser}
+          />
+        </div>
+
       </div>
     </div>
   );
--- treev2/src/components/UserProfile.jsx
+++ treev2/src/components/UserProfile.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import cn from 'classnames'
+import '../styles/UserProfile.css';
 
 let UserProfile = (props) => {
+    let clickHandler = (e) => {
+        if(e.currentTarget.innerHTML.indexOf('Нет информации') === -1 && e.currentTarget.classList.contains('clickedPerson') === false) {
+            props.setActiveUser(e.currentTarget)
+        }
+    }
+    
     return(
-        <div>
-            
+        <div className={cn('person',{'clickedPerson': props.isActive},
+                                    {'fillColor': props.illnesses.length > 0},
+                                    {'me': props.name === 'Я'}, 
+                                    {'father': props.name === 'Отец'},
+                                    {'mother': props.name === 'Мать'},
+                                    {'siblings': props.name === 'Братья, сестры'},
+                                    {'babushka': props.name === 'Бабушка'},
+                                    {'dedushka': props.name === 'Дедушка'},
+                                    {'babushkaFather': props.id === 4},
+                                    {'dedushkaFather': props.id === 5},
+                                    {'babushkaMother': props.id === 6},
+                                    {'dedushkaMother': props.id === 7},
+                                    {'fatherSiblings': props.id === 8},
+                                    {'motherSiblings': props.id === 9},
+                                    {'selfSiblings': props.id === 10})}
+              onClick={clickHandler}>
+            <div className='name'>
+              <p className='personTitle'>{props.name}</p>
+              <p className='personInfo'>{props.illnesses.length > 0 ? props.illnesses.length + ' заболевания' : 'Нет информации'}</p>
+            </div>
         </div>
     )
 }
